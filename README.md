3e54bae8f636474248b4c4f5b7927f4d7cc2fd38
Rocky Luvis <rockyinott24@yahoo.com>
2024-09-14 08:46:21 -0500

    Revert "Data transformation"
    This reverts commit 260d43a8746d97d4e30f8f896653a8f112a832ca.

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
 # deep_learning
-
-# Workflows
-- constants
-- config_entity
-- artifact_entity
-- components
-- pipeline
-- main
--- xray/constant/training_pipeline/__init__.py
+++ xray/constant/training_pipeline/__init__.py
@@ -10,43 +10,4 @@ ARTIFACT_DIR: str = "artifacts"
 BUCKET_NAME: str = "dlxrayproject"
 S3_DATA_FOLDER: str = "data"
 
-#Data Transformation constants
-
-CLASS_LABEL_1: str = "NORMAL"
-CLASS_LABEL_2: str = "PNEUMONIA"
-
-# Get these values from PyTorch Documentation
-BRIGHTNESS: int = 0.10
-CONTRAST: int = 0.1
-SATURATION: int = 0.10
-HUE: int = 0.1
-RESIZE: int = 224 #https://keras.io/api/applications/vgg/#vgg16-function
-CENTERCROP: int = 224 # Default image resolution taken by all models and it is represented as 224,224,3 
-#where 3 is number of channels - R G B
-RANDOMROTATION: int = 10
-NORMALIZE_LIST_1: List[int] = [0.485, 0.456, 0.406]
-NORMALIZE_LIST_2: List[int] = [0.229, 0.224, 0.225]
-TRAIN_TRANSFORMS_KEY: str = "xray_train_transforms"
-TRAIN_TRANSFORMS_FILE: str = "train_transforms.pkl"
-TEST_TRANSFORMS_FILE: str = "test_transforms.pkl"
-BATCH_SIZE: int = 2
-SHUFFLE: bool = False
-PIN_MEMORY: bool = True
-
-
-#model trainer constants
-
-TRAINED_MODEL_DIR: str = "trained_model"
-TRAINED_MODEL_NAME: str = "cvmodel.pt"
-
-DEVICE: torch.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-STEP_SIZE: int = 6
-GAMMA: int = 0.5
-EPOCH: int = 1
-
-BENTOML_MODEL_NAME: str = "xray_model"
-BENTOML_SERVICE_NAME: str = "xray_service"
-BENTOML_ECR_URI: str = "xray_bento_image"
-
-PREDICTION_LABEL: dict = {"0": CLASS_LABEL_1, 1: CLASS_LABEL_2}
+#Data Transformation
